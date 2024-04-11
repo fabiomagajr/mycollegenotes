cd6c51aa33e734125704473b4457d5847406a9e5
Fabio Magalhães <112334507+fabiomagajr@users.noreply.github.com>
2024-04-11 07:25:34 -0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function deuCerto(email, pass){
+    if(email=="admin" && pass=="1234"){
+        alert("Deu certo!")
+    }else{
+        alert("Credenciais inválidas.\nTente novamente.")
+    }
+
+
+    
+}
+
+function souBurro(){
+    alert("Pergunte ao Fabio como acessar\nObrigado!")
+}
