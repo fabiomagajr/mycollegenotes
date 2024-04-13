853cdf47b01f005addf8fd838879c0e51609eaff
Fabio Magalhães <112334507+fabiomagajr@users.noreply.github.com>
2024-04-13 20:47:27 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function deuCerto(email, pass){
     if(email=="admin" && pass=="1234"){
         alert("Deu certo!")
-        window.open("pages/home.html","_blank");
+        window.open(".pages/home.html","_blank");
         //openTab("pages/home.html","_blank");
     }else{
         alert("Credenciais inválidas.\nTente novamente.")
