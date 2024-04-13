1edbb30cab011e2c42c22ffd98490b26e8f6dd6e
Fabio Magalhães <112334507+fabiomagajr@users.noreply.github.com>
2024-04-13 20:39:02 -0300

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,9 @@
+function changeColor(btn,color){
+    if(btn){
+        document.getElementById('corpo').style.backgroundColor=black;
+    }else{
+      //  document.getElementById('corpo').style
+        //.background = linear-gradient(45deg,#7134db,#2ecc71);
+    }
+
+}
